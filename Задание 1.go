e664af907ec93036ae142f97ca074e2ae469591c
Lorenso <150370835+LorensoBaron@users.noreply.github.com>
2025-09-07 19:17:00 +0500

    Задание 1.go

--- Задание 1.go
+++ Задание 1.go
@@ -4,26 +4,23 @@ import(
 )
 
 func main() {
-    days := []int{ 2, 3, 4, 5, 6, 7, 4, 5} // Пример: 10 дней, включая все дни недели.
+    days := []int{ 2, 3, 4, 5, 6, 7, 4, 5} 
 
- // Цены
  weekdayPrice := 2100
  weekendPrice := 2850
 
  totalCost := 0
-
- // Перебираем дни и рассчитываем стоимость
  for _, day := range days {
   if day >= 1 && day <= 4 { // Пн, Вт, Ср, Чт
    totalCost += weekdayPrice
   } else if day >= 5 && day <= 7 { // Пт, Сб, Вс
    totalCost += weekendPrice
   } else {
-   fmt.Println("Ошибка: Некорректный день недели:", day) // Обработка ошибки (некорректный день)
-   return // Завершаем программу, если обнаружена ошибка
+   fmt.Println("Ошибка: Некорректный день недели:", day) 
+   return 
   }
  }
 
- // Выводим общую стоимость
  fmt.Println(totalCost)	
+
 }
