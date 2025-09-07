2379d92c3e40fcd2b85e6c336d17489c50616c8d
Lorenso <150370835+LorensoBaron@users.noreply.github.com>
2025-09-07 19:17:38 +0500

    Задание 2.go

--- Задание 2.go
+++ Задание 2.go
@@ -5,13 +5,14 @@ import(
 
 func main() {
     var mainBaggage, handLuggage, extraLuggage float64
-// основной багаж
+
     fmt.Scanln(&mainBaggage)
-// ручная кладь
+
     fmt.Scanln(&handLuggage)
-// доп ручная кладь
+
     fmt.Scanln(&extraLuggage)
 
     total := mainBaggage + handLuggage + extraLuggage
     fmt.Printf("Общий вес багажа: %.2f кг\n", total)
+
 }
